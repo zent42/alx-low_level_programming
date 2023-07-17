e9bfb8e447718c7f6bf3218c9e2312df0459d985
zent42 <zayad.hammani@gmail.com>
2023-07-17 10:45:46 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
-* main - prints the name of the file(filles > > output)
-*
-*Return: Always 0 (Success)
-*/
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf("This program is running from: %s\n", "UNIQUE_IDENTIFIER_FILE");
-return (0);
+	printf("This program is running from: %s\n", "UNIQUE_IDENTIFIER_FILE");
+
+	return (0);
 }
+
