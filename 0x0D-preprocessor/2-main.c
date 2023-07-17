6f96a49037d91bf3e8ea82ddfec96c0e461a9724
zent42 <zayad.hammani@gmail.com>
2023-07-17 09:57:22 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+
+main - prints a custom message indicating the current file name
+Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("Current file: %s\n", FILE);
+return (0);
+}
