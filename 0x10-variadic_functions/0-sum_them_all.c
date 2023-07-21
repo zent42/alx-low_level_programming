eb1bc599489bb524f014bdf176c86b17a7a7b5d0
zent42 <zayad.hammani@gmail.com>
2023-07-20 17:31:31 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,25 +1,23 @@
-#include <stdarg.h>
+#include "variadic_functions.h"
 /**
 * sum_them_all - calculates the sum of all its parameters
 * @n: number of arguments passed to the function
 *
 * Return: the resulting sum
 */
-int sum_them_all(const unsigned int n, ...)
+int calculate_sum(const unsigned int n, ...)
 {
-va_list args;
 unsigned int i;
 int sum = 0;
+va_list list;
 
-va_start(args, n);
+va_start(list, n);
 
-for (i = 0; i < n; i++) {
-sum += va_arg(args, int);
-}
+for (i = 0; i < n; i++)
+sum += va_arg(list, int);
 
-va_end(args);
+va_end(list);
 
 return (sum);
 }
-
 
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,35 +1,13 @@
+// my_variadic_functions.h
 #ifndef VAR_FUNC_H
 #define VAR_FUNC_H
+
 #include <stdio.h>
 #include <stdarg.h>
-/**
-* sum_varargs - Calculates the sum of all its parameters.
-* @n: The number of arguments passed to the function.
-*
-* Return: The resulting sum.
-*/
-int sum_varargs(const unsigned int n, ...);
 
-/**
-* print_numbers_varargs - Prints numbers separated by a given separator.
-* @separator: The separator string.
-* @n: The number of arguments passed to the function.
-*/
-void print_numbers_varargs(const char *separator, const unsigned int n, ...);
-
-/**
-* print_strings_varargs - Prints strings separated by a given separator.
-* @separator: The separator string.
-* @n: The number of arguments passed to the function.
-*/
-void print_strings_varargs(const char *separator, const unsigned int n, ...);
-
-/**
-* print_all_varargs - Prints various types based on the format specifier.
-* @format: The format string.
-* ...: Additional arguments depending on the format specifier.
-*/
-void print_all_varargs(const char * const format, ...);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
-
