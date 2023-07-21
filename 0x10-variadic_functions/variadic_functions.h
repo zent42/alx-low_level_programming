1ad7db68f9d8ecbe038376c354a2cb027c8538b9
zent42 <zayad.hammani@gmail.com>
2023-07-20 17:16:30 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,20 +1,35 @@
 #ifndef VAR_FUNC_H
 #define VAR_FUNC_H
-
 #include <stdio.h>
 #include <stdarg.h>
+/**
+* sum_varargs - Calculates the sum of all its parameters.
+* @n: The number of arguments passed to the function.
+*
+* Return: The resulting sum.
+*/
+int sum_varargs(const unsigned int n, ...);
 
-// Function to calculate the sum of all its parameters
-int sum_them_all(const unsigned int n, ...);
+/**
+* print_numbers_varargs - Prints numbers separated by a given separator.
+* @separator: The separator string.
+* @n: The number of arguments passed to the function.
+*/
+void print_numbers_varargs(const char *separator, const unsigned int n, ...);
 
-// Function to print numbers separated by a given separator
-void print_numbers(const char *separator, const unsigned int n, ...);
+/**
+* print_strings_varargs - Prints strings separated by a given separator.
+* @separator: The separator string.
+* @n: The number of arguments passed to the function.
+*/
+void print_strings_varargs(const char *separator, const unsigned int n, ...);
 
-// Function to print strings separated by a given separator
-void print_strings(const char *separator, const unsigned int n, ...);
-
-// Function to print various types based on the format specifier
-void print_all(const char * const format, ...);
+/**
+* print_all_varargs - Prints various types based on the format specifier.
+* @format: The format string.
+* ...: Additional arguments depending on the format specifier.
+*/
+void print_all_varargs(const char * const format, ...);
 
 #endif
 
