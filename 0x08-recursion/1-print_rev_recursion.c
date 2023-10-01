82474cdf88deec50c36eec49925d92bbde973098
zent42 <zayad.hammani@gmial.com>
2023-10-01 16:58:18 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
-/**
-* my_print_rev_recursion - prints a string in reverse
-* @str: the string to print in reverse
-*/
-void my_print_rev_recursion(char *str)
-{
-if (*str)
-{
-my_print_rev_recursion(str + 1);
-_putchar(*str);
-}
-}
 
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
