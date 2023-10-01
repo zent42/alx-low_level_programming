8e4b8d9daaeb0a1e4b897505aba7589f5e6d6d24
zent42 <zayad.hammani@gmial.com>
2023-10-01 16:34:17 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-* my_puts_recursion - custom function to print a string, followed by a new line
-* @str: string to print
-*/
-void my_puts_recursion(char *str)
+ * printStringRecursively - prints a string, followed by a new line
+ * @str: string to print
+ */
+void printStringRecursively(char *str)
 {
 if (*str == '\0')
 {
@@ -11,6 +11,6 @@ _putchar('\n');
 return;
 }
 _putchar(*str);
-my_puts_recursion(str + 1);
+printStringRecursively(str + 1);
 }
 
